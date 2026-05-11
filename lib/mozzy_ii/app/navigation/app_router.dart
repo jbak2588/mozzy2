2404fd76ac9e1eb61d3ea8ab67db4742dcdefb53
jbak2588 <jbak2588@gmail.com>
2026-05-11 12:47:52 +0700

    fix: add missing import for AccountDeletionScreen

--- lib/mozzy_ii/app/navigation/app_router.dart
+++ lib/mozzy_ii/app/navigation/app_router.dart
@@ -35,6 +35,7 @@ import '../../domains/monetization/screens/admin_monetization_audit_screen.dart'
 import '../../domains/moderation/screens/admin_moderation_screen.dart';
 import '../../domains/feedback/screens/feedback_screen.dart';
 import '../../domains/feedback/screens/admin_feedback_screen.dart';
+import '../../domains/account/screens/account_deletion_screen.dart';
 import '../../domains/feed/screens/smart_feed_screen.dart';
 import '../../core/config/beta_feature_flags.dart';
 import '../../shared/screens/feature_coming_soon_screen.dart';
